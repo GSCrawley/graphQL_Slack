8b002a7921cf62a6bdc4dfb24dcf7ecb6fb47b6f
Gideon Crawley <gscrawley@gmail.com>
2021-05-14 03:10:09 -0700

    moved everything into client and server folders

---
+++ server/server.js
@@ -0,0 +1,110 @@
+const { ApolloServer, gql, PubSub } = require('apollo-server');
+
+const pubsub = new PubSub();
+
+const channels = [ {
+	name: 'main',  
+	posts: [	
+        { message: 'Tnis is my main message, the obvious straightforward meaning of the words you hear coming out of my mouth', date: new Date() } ],
+	name: 'subtext', 
+    posts: [ 
+		{ message: 'This is the underlying subtext of my message, the hidden message hidden behind my message, that odds are you probably arent smart enough to be able to figure out', date: new Date() },
+	]
+},
+]
+
+const typeDefs = gql`
+	type Post {
+		message: String!
+    date: String
+  }
+
+	type Channel {
+    name: String!
+    posts: [Post!]
+  }
+
+	type Query {                            
+		posts(channel: String!): [Post!]!
+    channels: [Channel!]! 
+	}
+                                                                              
+  type Mutation {
+		addPost(channel: String!, message: String!): Post
+		addChannel(name: String!): Channel
+	}                                           
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+	type Subscription {
+		newPost(channel: String!): Post
+		newChannel: Channel!
+	}
+`
+
+const resolvers = {
+	Post: {
+        message: (parent) => {
+          return parent.message
+        },
+        date: (parent) => {
+          return new Date(parent.date).toLocaleDateString()
+        }
+      },
+	  Channel: {
+        name: (parent) => {
+          return parent.name
+        },
+        posts: (parent) => {
+          return parent.posts
+        } 
+      },
+	  Query: {
+		  posts: (_, { channel }) => { 
+			  return channels.filter(e => e.name === channel)[0].posts 
+         },
+        channels: () => { 
+            return channels
+        }
+    },
+	Mutation: {
+      addPost: (_, { channel, message  }) => {
+			    // console.log(message, channel)
+          const post = { message, date: new Date() }
+          const foundChannel = channels.find(i => i.name === channel)
+			    console.log(foundChannel)
+			    if (foundChannel === undefined){
+              return null
+          }    
+			    foundChannel.posts.push(post)
+          pubsub.publish('NEW_POST', { newPost: post })
+          return post
+      },
+		  addChannel: (_, { name }) => {
+          const channel = { 
+				    name, 
+				    posts: []
+			    }
+          pubsub.publish('NEW_CHANNEL', { newChannel: channel })
+          return channel
+      }
+  },
+	Subscription: {
+      newPost: {
+          subscribe: () => {
+                pubsub.asyncIterator('NEW_POST')
+      },
+      newChannel: {
+          subscribe: () => pubsub.asyncIterator('NEW_CHANNEL')
+        }
+  }
+}
+}
+
+const server = new ApolloServer({ 
+	typeDefs, 
+	resolvers 
+});
+
+// The `listen` method launches a web server.e
+server.listen().then(({ url }) => {
+	console.log(`🚀 Server ready at ${url}`);
+});
